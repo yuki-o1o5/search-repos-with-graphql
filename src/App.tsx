d36cb52d01a8dfa88f8efb1c3d102ac253d96dfc
yuki-o1o5 <jkksst1712@gmail.com>
2024-01-13 13:05:22 -0800

    feat: add form

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
-import { gql } from "apollo-boost";
 import "./App.css";
 import { useQuery } from "@apollo/client";
+import { SEARCH_REPOSITORIES } from "./graphql";
+import { useState } from "react";
 
-const ME = gql`
-  query me {
-    user(login: "yuki-o1o5") {
-      name
-      avatarUrl
-    }
-  }
-`;
+const INITIAL_VARIABLES = {
+  after: null,
+  before: null,
+  first: 5,
+  last: null,
+  query: "フロントエンドエンジニア",
+};
+
+type QueryVariables = {
+  after: string | null;
+  before: string | null;
+  first: number | null;
+  last: number | null;
+  query: string;
+};
 
 function App() {
-  const { loading, error, data } = useQuery(ME);
+  const [valiables, setValiables] = useState<QueryVariables>(INITIAL_VARIABLES);
+  const { loading, error, data } = useQuery(SEARCH_REPOSITORIES, {
+    variables: valiables,
+  });
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
+
+  const handleChangeQuery = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValiables({
+      ...valiables,
+      query: event.target.value,
+    });
+  };
+  console.log("query:", valiables.query);
   return (
     <>
       <div>Hello</div>
-      {data.user.name}
+      <form>
+        <input
+          type="text"
+          value={valiables.query}
+          onChange={handleChangeQuery}
+        />
+      </form>
+      {console.log(data)}
     </>
   );
 }
