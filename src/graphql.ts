e8350d3ca1505c13837356ac4796a1f27748a40e
Yuki Kasugai <108368307+yuki-o1o5@users.noreply.github.com>
2024-01-13 13:06:25 -0800

    feat: add form (#4)

---
+++ src/graphql.ts
@@ -0,0 +1,51 @@
+import { gql } from "apollo-boost";
+
+export const ME = gql`
+  query me {
+    user(login: "yuki-o1o5") {
+      name
+      avatarUrl
+    }
+  }
+`;
+
+export const SEARCH_REPOSITORIES = gql`
+  query searchRepositories(
+    $first: Int
+    $after: String
+    $last: Int
+    $before: String
+    $query: String!
+  ) {
+    search(
+      first: $first
+      after: $after
+      last: $last
+      before: $before
+      query: $query
+      type: REPOSITORY
+    ) {
+      repositoryCount
+      pageInfo {
+        endCursor
+        hasNextPage
+        hasPreviousPage
+        startCursor
+      }
+      edges {
+        cursor
+        node {
+          ... on Repository {
+            id
+            name
+            url
+            stargazers {
+              totalCount
+            }
+            viewerHasStarred
+          }
+        }
+      }
+    }
+  }
+`;
